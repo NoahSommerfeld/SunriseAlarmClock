5bf4ea3f96a5b4b3ae7c4bb6bd6f8288358ab94b
maestro_master <noah.sommerfeld@gmail.com>
2020-12-09 17:13:21 -0800

    Corrected so the comments and code align

--- Sunrise/Config-TEMPLATE.h
+++ Sunrise/Config-TEMPLATE.h
@@ -1,5 +1,5 @@
 #define CONFIG_WIFI_SSID "Your home wifi SSID"
 #define CONFIG_WIFI_PASSWORD "Your_WIFI_Password_Here"
 
-#define CONFIG_SUNRISE_START_HOUR "6" //hour to start. 24 hr clock, don't use leading 0's. 
-#define CONFIG_SUNRISE_START_MINUTE "45" //starts the sunrise at 5:50am. 
+#define CONFIG_SUNRISE_START_HOUR "5" //hour to start. 24 hr clock, don't use leading 0's. 
+#define CONFIG_SUNRISE_START_MINUTE "50" //starts the sunrise at 5:50am. 
